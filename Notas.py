30dcbce61bd0bd2109c59ae5a4fce31cf595e5a7
RC082 <105440868+RC082@users.noreply.github.com>
2022-05-19 19:12:30 -0300

    Add files via upload

---
+++ Notas.py
@@ -0,0 +1,31 @@
+import time 
+
+time.sleep(2)
+nota1 = int(input("Digite sua nota da primeira avaliação: "))
+time.sleep(1)
+nota2 = int(input("Digite sua nota da segunda avaliação: "))
+time.sleep(1)
+nota3 = int(input("Digite sua nota da terceira avaliação: "))
+time.sleep(1)
+nota4 = int(input("Digite sua nota da quarta avaliação: "))
+time.sleep(1)
+print("Calculando as suas notas...")
+time.sleep(2)
+
+multi1 = nota1 * 2
+multi2 = nota2 * 3
+multi3 = nota3 * 2
+multi4 = nota4 * 3
+
+s = multi1 + multi2 + multi3 + multi4
+
+if s <= 50:
+    print("Você não passou 😭")
+
+if s >= 50:
+    print("Você passou, Parabéns 😄")
+
+
+if s == 50:
+    print("Você passou, Parabéns 😄")
+    
