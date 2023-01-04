179d592903fed5580c204510999890d559442cfa
Roberto <105440868+RC082@users.noreply.github.com>
2023-01-04 18:24:43 -0300

    Add files via upload

--- Notas.py
+++ Notas.py
@@ -1,5 +1,35 @@
 import time 
 
+print("""
+
+                                                                                                                                                    
+                                                                                                                                                    
+NNNNNNNN        NNNNNNNN                          tttt                                                                                              
+N:::::::N       N::::::N                       ttt:::t                                                                                              
+N::::::::N      N::::::N                       t:::::t                                                                                              
+N:::::::::N     N::::::N                       t:::::t                                                                                              
+N::::::::::N    N::::::N   ooooooooooo   ttttttt:::::ttttttt      aaaaaaaaaaaaa      ssssssssss           ppppp   pppppppppyyyyyyy           yyyyyyy
+N:::::::::::N   N::::::N oo:::::::::::oo t:::::::::::::::::t      a::::::::::::a   ss::::::::::s          p::::ppp:::::::::py:::::y         y:::::y 
+N:::::::N::::N  N::::::No:::::::::::::::ot:::::::::::::::::t      aaaaaaaaa:::::ass:::::::::::::s         p:::::::::::::::::py:::::y       y:::::y  
+N::::::N N::::N N::::::No:::::ooooo:::::otttttt:::::::tttttt               a::::as::::::ssss:::::s        pp::::::ppppp::::::py:::::y     y:::::y   
+N::::::N  N::::N:::::::No::::o     o::::o      t:::::t              aaaaaaa:::::a s:::::s  ssssss          p:::::p     p:::::p y:::::y   y:::::y    
+N::::::N   N:::::::::::No::::o     o::::o      t:::::t            aa::::::::::::a   s::::::s               p:::::p     p:::::p  y:::::y y:::::y     
+N::::::N    N::::::::::No::::o     o::::o      t:::::t           a::::aaaa::::::a      s::::::s            p:::::p     p:::::p   y:::::y:::::y      
+N::::::N     N:::::::::No::::o     o::::o      t:::::t    tttttta::::a    a:::::assssss   s:::::s          p:::::p    p::::::p    y:::::::::y       
+N::::::N      N::::::::No:::::ooooo:::::o      t::::::tttt:::::ta::::a    a:::::as:::::ssss::::::s         p:::::ppppp:::::::p     y:::::::y        
+N::::::N       N:::::::No:::::::::::::::o      tt::::::::::::::ta:::::aaaa::::::as::::::::::::::s  ......  p::::::::::::::::p       y:::::y         
+N::::::N        N::::::N oo:::::::::::oo         tt:::::::::::tt a::::::::::aa:::as:::::::::::ss   .::::.  p::::::::::::::pp       y:::::y          
+NNNNNNNN         NNNNNNN   ooooooooooo             ttttttttttt    aaaaaaaaaa  aaaa sssssssssss     ......  p::::::pppppppp        y:::::y           
+                                                                                                           p:::::p               y:::::y            
+                                                                                                           p:::::p              y:::::y             
+                                                                                                          p:::::::p            y:::::y              
+                                                                                                          p:::::::p           y:::::y               
+                                                                                                          p:::::::p          yyyyyyy                
+                                                                                                          ppppppppp                                 
+                                                                                                                                                    
+""")
+
+
 time.sleep(2)
 nota1 = int(input("Digite sua nota da primeira avaliação: "))
 time.sleep(1)
